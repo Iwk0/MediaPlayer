bb13e5ea8b88e527b7c74e25a31ac1ff820c790f
Iwk0 <ivomishev@gmail.com>
2014-10-22 17:37:03 +0300

    New

--- app/src/main/java/com/mediaplayer/utils/Constants.java
+++ app/src/main/java/com/mediaplayer/utils/Constants.java
@@ -7,6 +7,7 @@ import android.os.Environment;
  */
 public interface Constants {
 
+    int DATABASE_VERSION = 30;
     String TRACKS_PATH = "tracks path";
     String TRACK_PATH = "track path";
     String TRACK_NAME = "track name";
@@ -14,19 +15,14 @@ public interface Constants {
     String SAVE_RANDOM_MODE = "random mode";
     String RECENTLY_PLAYED = "recently played";
     String STORAGE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-    int DATABASE_VERSION = 30;
     String DATABASE_NAME = "recentlyPlayedTracks";
     String TABLE_NAME = "paths";
     String COLUMN_ID = "id";
     String COLUMN_PATH = "path";
     String COLUMN_NAME = "name";
-
     String PREF_SETTINGS = "preference settings";
-
     String ID = "id";
     String PATH = "path";
     String IMAGE = "image";
-
     String REDIRECT_IN_LIST_VIEW = "redirect list view";
 }
